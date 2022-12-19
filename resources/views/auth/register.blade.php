e3c4b74b6abccac4d7079a8709d72159a903d9fe
David Duran Vallejo <davidstevend@hotmail.com>
2022-12-18 23:14:17 -0500

    Formulario de autenticacion. Validaciones de Contraseña

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -25,12 +25,10 @@
                             </div>
                         </div>
                         <div class="row mb-3">
-                            <label for="last_name" class="col-md-4 col-form-label text-md-end">{{ __('Apellido') }}</label>
+                            <label for="last_name" class="col-md-4 col-form-label text-md-end">Apellido</label>
 
                             <div class="col-md-6">
-                                <input id="last_name" type="text" class="form-control @error('last_name') is-invalid @enderror" name="last_name
-                                last_name
-                                last_name" value="{{ old('last_name') }}" required autocomplete="last_name" autofocus>
+                                <input id="last_name" type="text" class="form-control @error('last_name') is-invalid @enderror" name="last_name" value="{{ old('last_name') }}" required autocomplete="last_name" autofocus>
 
                                 @error('last_name')
                                     <span class="invalid-feedback" role="alert">
