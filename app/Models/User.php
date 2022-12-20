1d65997d6003591f97fd3df25b3fd7f42ff5dc92
David Duran Vallejo <davidstevend@hotmail.com>
2022-12-20 10:55:33 -0500

    Notificaciones en Plantilla. - muestra solo las notificaciones no terminadas

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,11 +2,13 @@
 
 namespace App\Models;
 
+use App\Models\Task;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
@@ -61,5 +63,20 @@ class User extends Authenticatable
         } else {
             return false;
         }
+    }
+
+    public function userNotifications()
+    {
+        return  Task::whereUserId(Auth::user()->id)->count();
+    }
+    public function userTaskPending()
+    {
+        return  Task::whereUserIdAndFinished(Auth::user()->id,'NO')->count();
+    }
+
+    public function userTasks()
+    {
+        $taskPending = Task::whereUserIdAndFinished(Auth::user()->id,'NO')->get();
+        return  $taskPending;
     }
 }
--- resources/views/layouts/plantilla.blade.php
+++ resources/views/layouts/plantilla.blade.php
@@ -67,8 +67,7 @@
                                 class="ft-menu font-large-1"></i></a></li>
                     <li class="nav-item mr-auto">
                         <a class="navbar-brand" href="{{ route('home') }}">
-                            <img class="brand-logo" alt="modern admin logo"
-                                src="{{ asset('logos/tua.png') }}">
+                            <img class="brand-logo" alt="modern admin logo" src="{{ asset('logos/tua.png') }}">
                             <h3 class="brand-text">TORRENTS U.</h3>
                         </a>
                     </li>
@@ -234,113 +233,65 @@
                                         <img src="{{ asset('app-assets/images/portrait/small/doctor.png') }}"
                                             alt="avatar"><i></i></span>
                                 </a>
-                                <div class="dropdown-menu dropdown-menu-right"><a class="dropdown-item" href="#"><i class="ft-user"></i> Editar Perfil</a>
+                                <div class="dropdown-menu dropdown-menu-right"><a class="dropdown-item" href="#"><i
+                                            class="ft-user"></i> Editar Perfil</a>
 
-                                <div class="dropdown-divider"></div><a class="dropdown-item"
-                                    href="{{ url('/logout') }}"><i class="ft-power"></i> Logout</a>
-                    </div>
-                    </li>
-
-
-
-                    <li class="dropdown dropdown-notification nav-item">
-                        <a class="nav-link nav-link-label" href="#" data-toggle="dropdown"><i
-                                class="ficon ft-bell"></i>
-                            <span
-                                class="badge badge-pill badge-default badge-danger badge-default badge-up badge-glow">1</span>
-                        </a>
-                        <ul class="dropdown-menu dropdown-menu-media dropdown-menu-right">
-                            <li class="dropdown-menu-header">
-                                <h6 class="dropdown-header m-0">
-                                    <span class="grey darken-2">Notifications</span>
-                                </h6>
-                                <span class="notification-tag badge badge-default badge-danger float-right m-0">11
-                                    New</span>
+                                    <div class="dropdown-divider"></div><a class="dropdown-item"
+                                        href="{{ url('/logout') }}"><i class="ft-power"></i> Logout</a>
+                                </div>
                             </li>
-                            <li class="scrollable-container media-list w-100">
-                                <a href="javascript:void(0)">
-                                    <div class="media">
-                                        <div class="media-left align-self-center"><i
-                                                class="ft-plus-square icon-bg-circle bg-cyan"></i></div>
-                                        <div class="media-body">
-                                            <h6 class="media-heading">You have new order!</h6>
-                                            <p class="notification-text font-small-3 text-muted">Lorem ipsum dolor sit
-                                                amet, consectetuer elit.</p>
-                                            <small>
-                                                <time class="media-meta text-muted"
-                                                    datetime="2015-06-11T18:29:20+08:00">30 minutes ago</time>
-                                            </small>
-                                        </div>
-                                    </div>
+
+
+
+                            <li class="dropdown dropdown-notification nav-item">
+                                <a class="nav-link nav-link-label" href="#" data-toggle="dropdown"><i
+                                        class="ficon ft-bell"></i>
+                                    <span class="badge badge-pill badge-default badge-danger badge-default badge-up badge-glow"> {{ Auth::user()->userTaskPending() }}</span>
                                 </a>
-                                <a href="javascript:void(0)">
-                                    <div class="media">
-                                        <div class="media-left align-self-center"><i
-                                                class="ft-download-cloud icon-bg-circle bg-red bg-darken-1"></i></div>
-                                        <div class="media-body">
-                                            <h6 class="media-heading red darken-1">99% Server load</h6>
-                                            <p class="notification-text font-small-3 text-muted">Aliquam tincidunt mauris
-                                                eu risus.</p>
-                                            <small>
-                                                <time class="media-meta text-muted"
-                                                    datetime="2015-06-11T18:29:20+08:00">Five hour ago</time>
-                                            </small>
-                                        </div>
-                                    </div>
-                                </a>
-                                <a href="javascript:void(0)">
-                                    <div class="media">
-                                        <div class="media-left align-self-center"><i
-                                                class="ft-alert-triangle icon-bg-circle bg-yellow bg-darken-3"></i></div>
-                                        <div class="media-body">
-                                            <h6 class="media-heading yellow darken-3">Warning notifixation</h6>
-                                            <p class="notification-text font-small-3 text-muted">Vestibulum auctor dapibus
-                                                neque.</p>
-                                            <small>
-                                                <time class="media-meta text-muted"
-                                                    datetime="2015-06-11T18:29:20+08:00">Today</time>
-                                            </small>
-                                        </div>
-                                    </div>
-                                </a>
-                                <a href="javascript:void(0)">
-                                    <div class="media">
-                                        <div class="media-left align-self-center"><i
-                                                class="ft-check-circle icon-bg-circle bg-cyan"></i></div>
-                                        <div class="media-body">
-                                            <h6 class="media-heading">Complete the task</h6>
-                                            <small>
-                                                <time class="media-meta text-muted"
-                                                    datetime="2015-06-11T18:29:20+08:00">Last week</time>
-                                            </small>
-                                        </div>
-                                    </div>
-                                </a>
-                                <a href="javascript:void(0)">
-                                    <div class="media">
-                                        <div class="media-left align-self-center"><i
-                                                class="ft-file icon-bg-circle bg-teal"></i></div>
-                                        <div class="media-body">
-                                            <h6 class="media-heading">Generate monthly report</h6>
-                                            <small>
-                                                <time class="media-meta text-muted"
-                                                    datetime="2015-06-11T18:29:20+08:00">Last month</time>
-                                            </small>
-                                        </div>
-                                    </div>
-                                </a>
+                                <ul class="dropdown-menu dropdown-menu-media dropdown-menu-right">
+                                    <li class="dropdown-menu-header">
+                                        <h6 class="dropdown-header m-0">
+                                            <span class="grey darken-2">todas las Tareas</span>
+                                        </h6>
+                                        <span class="notification-tag badge badge-default badge-danger float-right m-0"> 
+                                          {{ Auth::user()->userNotifications() }}
+                                         
+                                        </span>
+                                    </li>
+                                    <li class="scrollable-container media-list w-100">
+                                     
+                                        
+                                        {{-- Auth::user()->userTasks() --}}
+                                       @foreach(Auth::user()->userTasks() as $task) 
+                                          <a href="javascript:void(0)">
+                                            <div class="media">
+                                                <div class="media-left align-self-center"><i
+                                                        class="ft-alert-triangle icon-bg-circle bg-yellow bg-darken-3"></i>
+                                                </div>
+                                                <div class="media-body">
+                                                    <h6 class="media-heading yellow darken-3">{{ $task->task }}</h6>
+                                                    <p class="notification-text font-small-3 text-muted">{{ $task->description }}</p>
+                                                    <small>
+                                                        <time class="media-meta text-muted"
+                                                            datetime="2015-06-11T18:29:20+08:00">{{ $task->expiration_date }}</time>
+                                                    </small>
+                                                </div>
+                                            </div>
+                                        </a>
+                                        @endforeach                                   
+                                      
+                                    </li>
+                                    <li class="dropdown-menu-footer"><a class="dropdown-item text-muted text-center"
+                                      href="{{ url('tasks') }}">Ver Todas las Tareas</a></li>
+                                </ul>
                             </li>
-                            <li class="dropdown-menu-footer"><a class="dropdown-item text-muted text-center"
-                                    href="javascript:void(0)">Read all notifications</a></li>
+
                         </ul>
-                      </li>
-                      
-                    </ul>
 
 
-                @endauth
+                    @endauth
 
-            </div>
+                </div>
     </nav>
     <!-- /////////////////////////////menu///////////////////////////////////////////////-->
     @auth
